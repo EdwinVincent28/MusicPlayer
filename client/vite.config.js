14e2e916abe6a88d64120be82b0fd43d9a73ee52
EdwinVincent28 <edwinvincent.evt@gmail.com>
2026-03-12 20:30:46 +0100

    render backend proxy updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:4000',
-      "/images": "http://localhost:4000",
+      '/api': 'https://musicplayer-backend-ddnr.onrender.com',
+      "/images": "https://musicplayer-backend-ddnr.onrender.com",
   }}
 })
